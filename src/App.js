d40ed8cd74e99111d31281cc2e2ff0e5a71342b7
Neda Rohani <sonia@Nedas-MacBook-Pro.local>
2020-01-30 12:28:13 -0800

    get data using axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import './App.css'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items : []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/data.json')
+      .then(res => {
+        console.log(res)
+        this.setState({items: res.data}, () => {console.log(this.state.items)})
+    })
+  }
+
   render() {
-    return null
+    return <div>test</div>
   }
 }
